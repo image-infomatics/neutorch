186a3577b8a50b53c957dd650ec8176b17ebbff2
quantumanni <mannypaezjr@comcast.net>
2023-10-13 14:08:07 -0400

    second changes to boundary_augmentationdataset

--- neutorch/train/boundary_aug.py
+++ neutorch/train/boundary_aug.py
@@ -1,21 +1,25 @@
+import os
 from functools import cached_property
 
-#import os
 import click
 from yacs.config import CfgNode
-import torch
-import torch.distributed as dist
 
 from neutorch.data.dataset import BoundaryAugmentationDataset
 from .base import TrainerBase, setup, cleanup
 
-class BoundaryAugTrainer(TrainerBase):
-    def __init__(self, cfg: CfgNode) -> None:
-        assert isinstance(cfg, CfgNode) 
-        super().__init__(cfg) 
+from .base import TrainerBase
 
-        self.cfg = cfg
-        #self.num_classes = self.cfg.model.out_channels
+import torch
+import torch.distributed as dist
+
+class BoundaryAugTrainer(TrainerBase):
+    def __init__(self, cfg: CfgNode,
+                 device: torch.DeviceObjType=None,
+                 local_rank: int = int(os.getenv('LOCAL_RANK', -1)) 
+        ) -> None:
+        assert isinstance(cfg, CfgNode)
+        super().__init__(cfg, device=device)
+        #self.cfg = cfg
 
     @cached_property
     def training_dataset(self):
@@ -34,8 +38,21 @@ class BoundaryAugTrainer(TrainerBase):
     default='./config.yaml', 
     help = 'configuration file containing all the parameters.'
 )
-def main(config_file: str):  
+@click.option('--local-rank', '-r',
+    type=click.INT, default=int(os.getenv('LOCAL_RANK', -1)),
+    help='rank of local process. It is used to assign batches and GPU devices.'
+)
+def main(config_file: str, local_rank: int):  
+    if local_rank != -1:
+        dist.init_process_group(backend="nccl", init_method='env://')
+        print(f'local rank of processes: {local_rank}')
+        torch.cuda.set_device(local_rank)
+        device=torch.device("cuda", local_rank)
+    else:
+        setup()
+
     from neutorch.data.dataset import load_cfg
     cfg = load_cfg(config_file)
-    trainer = BoundaryAugTrainer(cfg) #, device=device)
+    trainer = BoundaryAugTrainer(cfg, device=device)
     trainer()
+    cleanup()
