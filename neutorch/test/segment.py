4c73204cb2b85bfccccb029654f959a1ac212943
mannypaeza <mpaez@rustyamd1.cm.cluster>
2024-03-19 21:45:09 -0400

    waterz working

--- neutorch/test/segment.py
+++ neutorch/test/segment.py
@@ -24,10 +24,10 @@ class segment_methodology():
             affs = load_chunk_or_volume(aff_path, **kwargs) 
             gt = load_chunk_or_volume(gt_path, **kwargs) 
  
-            affs_array = affs.array.astype(np.uint32)
+            affs_array = affs.array.astype(np.float32)
             gt_array = gt.array.astype(np.uint32)
             assert affs.shape[-3:] == gt.shape[-3:]
-            assert affs_array.dtype == gt_array.dtype
+            #assert affs_array.dtype == gt_array.dtype
 
             segmentation = wz.agglomerate(affs_array, seg_thresholds, gt=gt_array, 
                                           fragments=None, aff_threshold_low=0.0001, 
@@ -61,7 +61,7 @@ if __name__ == '__main__':
     affs_paths = ["/mnt/home/mpaez/ceph/affsmaptrain/sample2/affstrain2_28000_vol3.h5",]
     gt_paths = ["/mnt/ceph/users/neuro/wasp_em/jwu/40_gt/12_wasp_sample2/vol_07338/seg_v1.h5",]
 
-    segmentation = segment_methodology.evaluate(affs_paths, gt_paths) 
+    segmentation = segment_methodology.agglomerate(affs_paths, gt_paths) 
 
     for seg in segmentation:
         breakpoint() 
