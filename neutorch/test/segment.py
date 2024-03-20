49392f6fc4e7b64fa52d1d36d8b445c34d77f9e6
Manuel Paez <79882298+mannypaeza@users.noreply.github.com>
2024-03-19 20:11:07 -0400

    fixing dtype

--- neutorch/test/segment.py
+++ neutorch/test/segment.py
@@ -19,14 +19,16 @@ class segment_methodology():
     def agglomerate(self, affs_paths, gt_paths, **kwargs):
         segmentations = []
         
-        # aff_thresholds = [0.005, 0.995]
-        seg_thresholds = [0.1, 0.2, 0.3, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
-
+        seg_thresholds = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
         for aff_path, gt_path in zip(affs_paths, gt_paths): 
             affs = load_chunk_or_volume(aff_path, **kwargs) 
             gt = load_chunk_or_volume(gt_path, **kwargs) 
 
             assert affs.shape[-3:] == gt.shape[-3:]
+
+            gt_array = gt.array.astype('float32')
+            assert affs.shape[-3:] == gt.shape[-3:]
+            assert affs.array.dtype == gt_array.dtype
 
             segmentation = wz.agglomerate(affs.array, seg_thresholds, gt=gt.array, 
                                           fragments=None, aff_threshold_low=0.0001, 
@@ -41,14 +43,15 @@ class segment_methodology():
     def evaluate(self, affs_paths, gt_paths, **kwargs):
         segmentations = []
         
-        seg_thresholds = [0.1, 0.3, 0.6]
         for aff_path, gt_path in zip(affs_paths, gt_paths): 
             affs = load_chunk_or_volume(aff_path, **kwargs) 
             gt = load_chunk_or_volume(gt_paths, **kwargs) 
 
+            gt_array = gt.array.astype('float32')
             assert affs.shape[-3:] == gt.shape[-3:]
+            assert affs.array.dtype == gt_array.dtype
 
-            segmentation = wz.evaluate(affs.array, gt.array)
+            segmentation = wz.evaluate(affs.array, gt_array)
             segmentations.append(segmentation) 
 
         return segmentations
