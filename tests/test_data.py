a78bb805ee12a431e9f5dbd5a245ced66e3a5a87
xiuliren <7651573+xiuliren@users.noreply.github.com>
2025-05-08 17:52:43 +0800

    add test data

---
+++ tests/test_data.py
@@ -0,0 +1,41 @@
+# def test_sample():
+
+#%% 
+
+import os
+from tqdm import tqdm
+from chunkflow.lib.cartesian_coordinate import Cartesian
+from chunkflow.volume import load_chunk_or_volume
+
+from neutorch.data.dataset import load_cfg
+from neutorch.data.sample import SemanticSample
+
+PATCH_NUM = 100
+DEFAULT_PATCH_SIZE=Cartesian(1, 64, 64)
+OUT_DIR = os.path.expanduser('/tmp/patches/')
+cfg = load_cfg('/home/jwu/projects/63_hcs-image-analysis/42-incucyte-images/sample.yaml')
+
+# sample = SemanticSample.from_config_v6(cfg)
+
+def load_chunks_or_volumes(paths: List[str]):
+    inputs = []
+    for image_path in paths:
+        image_vol = load_chunk_or_volume(image_path)
+        inputs.append(image_vol)
+    return inputs 
+
+# %%
+for chunk_name, chunk_meta in cfg.items():
+    print(chunk_name)
+    images = []
+    for image_fname in chunk_meta['images']:
+        image_path = os.path.join(chunk_meta['dir'], image_fname)
+        image_vol = load_chunk_or_volume(image_path)
+        images.append(image_vol)
+
+    label_path = os.path.join(chunk_meta['dir'], chunk_meta['label'])
+    label = load_chunk_or_volume(label_path)
+    labels = [label]
+
+    
+# %%
